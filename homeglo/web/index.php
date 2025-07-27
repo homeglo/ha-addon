8f3e233c58689a62a67ebb5a88dba85624510f90
TimK <timk@TimKs-MacBook-Pro.local>
2025-07-26 23:33:08 -0400

    path

--- homeglo/web/index.php
+++ homeglo/web/index.php
@@ -8,12 +8,27 @@ ini_set('error_reporting','E_ALL & ~E_NOTICE & ~E_DEPRECATED');
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
-// Fix for Home Assistant ingress
+// Fix for Home Assistant ingress - ensure SCRIPT_NAME is set properly
 if (!empty($_SERVER['HTTP_X_INGRESS_PATH'])) {
     $_SERVER['SCRIPT_NAME'] = $_SERVER['HTTP_X_INGRESS_PATH'] . '/index.php';
+    $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
     
-    // Keep REQUEST_URI as-is for Yii to handle properly
-    // The baseUrl in config will handle the ingress path
+    // Fix REQUEST_URI to remove ingress path for Yii routing
+    $originalUri = $_SERVER['REQUEST_URI'] ?? '';
+    $ingressPath = $_SERVER['HTTP_X_INGRESS_PATH'];
+    
+    // Remove ingress path from request URI
+    if (strpos($originalUri, $ingressPath) === 0) {
+        $_SERVER['REQUEST_URI'] = substr($originalUri, strlen($ingressPath));
+    }
+    
+    // Remove /index.php if present
+    $_SERVER['REQUEST_URI'] = preg_replace('/^\/index\.php/', '', $_SERVER['REQUEST_URI']);
+    
+    // If empty, set to /
+    if (empty($_SERVER['REQUEST_URI'])) {
+        $_SERVER['REQUEST_URI'] = '/';
+    }
 }
 
 require __DIR__ . '/../vendor/autoload.php';
